9362741f86bd6fd06d8588647c617de140792e18
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-22 20:42:53 +0800

    add export config file

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,4 +1,7 @@
 
 
+# =================================
+# ****** @Plugin zap-zsh/vim ******
+# =================================
 # will escape back into normal mode upon pressing jk.
 export VI_MODE_ESC_INSERT="jk"
