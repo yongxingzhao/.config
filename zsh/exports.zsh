25fc4c795fae3ac35afe67ff387edbdd4acfc9f0
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-23 01:25:42 +0800

    modified exports.zsh

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,12 @@
-
-
 # =================================
 # ****** @Plugin zap-zsh/vim ******
 # =================================
 # will escape back into normal mode upon pressing jk.
 export VI_MODE_ESC_INSERT="jk"
+
+
+# =================================
+# ****** @Theme starship ******
+# =================================
+# wilmal, blazing-fast, and infinitely customizable prompt for any shell!
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
