47a70176f5666c0b5fce3c65e394106178c32775
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-23 01:25:25 +0800

    modified mappings.zsh

--- zsh/mappings.zsh
+++ zsh/mappings.zsh
@@ -3,3 +3,10 @@
 # =================================
 # Set editor default keymap to emacs (`-e`) or vi (`-v`)
 bindkey -v
+
+
+# ===================================================
+# ****** @Plugin zsh-users/zsh-autosuggestions ******
+# ===================================================
+# lears the current suggestion.
+bindkey '^k' autosuggest-clear
