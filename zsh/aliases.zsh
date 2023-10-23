915a96875a362750116e6ab10e4ae92b86dbdaef
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-24 00:26:17 +0800

    add alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,9 @@
 # ****** @Command tmux ******
 # ===========================
 alias ta="tmux attach -t"
+alias tn="tmux new -s"
+alias tls="tmux ls"
+alias tks="tmux kill-session -t"
 
 # ===========================
 # ****** @Command lazygit ******
