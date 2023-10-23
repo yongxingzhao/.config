14fadc89c5ae699924e55ee898033fa158d56d06
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-24 00:26:40 +0800

    add env

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,14 @@
 # =================================
+# ****** @User SDKMAN ******
+# =================================
+#
+export SDKMAN_DIR="/opt/sdkman"
+
+
+# =================================
 # ****** @Plugin zap-zsh/vim ******
 # =================================
+#
 # will escape back into normal mode upon pressing jk.
 export VI_MODE_ESC_INSERT="jk"
 
@@ -8,5 +16,22 @@ export VI_MODE_ESC_INSERT="jk"
 # =================================
 # ****** @Theme starship ******
 # =================================
+#
 # wilmal, blazing-fast, and infinitely customizable prompt for any shell!
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+
+
+# =====================
+# ****** @System ******
+# =====================
+#
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='mvim'
+fi
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# export PATH=$(echo $PATH | tr : "\n"| sort | uniq | tr "\n" :)
