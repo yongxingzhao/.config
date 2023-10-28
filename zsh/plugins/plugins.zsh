5253272e983d72e9b85bde6cf4811fa5b7cb7d71
ZhaoYongXing <yongxingzhao@foxmail.com>
2023-10-29 00:02:34 +0800

    add submodule for zsh-sdkman
    add submodule
    
    s
    
    s

--- zsh/plugins/plugins.zsh
+++ zsh/plugins/plugins.zsh
@@ -1,2 +1,5 @@
+# sdkman
+[[ -e $HOME/.config/zsh/plugins/zsh-sdkman/zsh-sdkman.plugin.zsh ]] && source "$HOME/.config/zsh/plugins/zsh-sdkman/zsh-sdkman.plugin.zsh"
+
 # iterm2_shell_integration
 [[ -e $HOME/.config/iterm2/.iterm2_shell_integration.zsh ]] && source "$HOME/.config/iterm2/.iterm2_shell_integration.zsh" || true
